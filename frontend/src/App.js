dd3aea9291908c8ce8965a50df0f85a492edf5c2
ATLH <s-attilah@hotmail.com>
2022-01-16 15:15:53 +0100

    test3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 const axios = require("axios");
 
 const App = () => {
-  console.log(process.env.REACT_APP_NAME, "cat nameeennnnnnnn!");
+  console.log(process.env.REACT_APP_NAME, "Bobby");
 
   const [user, setUser] = useState();
   useEffect(() => {
