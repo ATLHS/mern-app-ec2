cb79b8732d6916744527e4098bb9356232a429b9
ATLH <s-attilah@hotmail.com>
2022-01-18 17:41:22 +0100

    test

--- aws-script/mvbuildreactfolder.sh
+++ aws-script/mvbuildreactfolder.sh
@@ -3,7 +3,14 @@
 # install dependencies in front and back npm i
 
 # building react app
-echo "Building React Project"
-cd "/home/ubuntu/folder/frontend/"
-npm run build
+#echo "Building React Project"
+#cd "/home/ubuntu/folder/frontend"
+#npm run build
+
+# copy paste react build folder to nginx web server new location
+cp -r "/home/ubuntu/folder/frontend/build/"* "/var/www/react-app"
+
+# restart nginx
+cd "/home/ubuntu"
+sudo systemctl restart nginx
 
