3d999463f4b166611ecbf47eb2fe5175b7b76718
ATLH <s-attilah@hotmail.com>
2022-01-18 06:54:41 +0100

    add new config command in sh script for codedeploy

--- aws-script/mvbuildreactfolder.sh
+++ aws-script/mvbuildreactfolder.sh
@@ -2,8 +2,7 @@
 
 # building react app
 echo "Building React Project"
-cd "/home/ubuntu/folder/frontend/" 
-sudo npm run build
+cd "/home/ubuntu/folder/frontend/" && sudo npm run build
 
 # copy paste react build folder to nginx web server new location
 cp -r "/home/ubuntu/folder/frontend/build/"* "/var/www/react-app"
