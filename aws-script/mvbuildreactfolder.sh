a1245bf526215f4aad16e88f517ca0af73a196c3
ATLH <s-attilah@hotmail.com>
2022-01-19 04:49:50 +0100

    test

--- aws-script/mvbuildreactfolder.sh
+++ aws-script/mvbuildreactfolder.sh
@@ -1,7 +1,5 @@
 #!/bin/bash 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+sudo chown -R ubuntu "/home/ubuntu/folder/"
 
 # Building react app
 # cd "/home/ubuntu/folder/frontend"
